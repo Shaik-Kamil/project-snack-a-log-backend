5a3dda7b115cddb8316ef5d98e35148206a38b7c
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-02-02 11:22:50 -0500

    fixed route error

--- controllers/snackController.js
+++ controllers/snackController.js
@@ -72,7 +72,7 @@ snacks.delete('/:id', async (req, res) => {
 snacks.put('/:id', checkName, checkBoolean, validateImage, async (req, res) => {
   try {
     const { id } = req.params;
-    const updatedSnack = await updateSnack(id);
+    const updatedSnack = await updateSnack(id, req.body);
     res.status(200).json(updatedSnack);
   } catch (error) {
     res.status(404).json({ error: ' snack not found' });
