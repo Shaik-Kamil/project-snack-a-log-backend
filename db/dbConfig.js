af05489a74d664a8194fb51be1f13b4a206c8363
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-02-02 16:45:45 -0500

    changes to dbconfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -7,11 +7,11 @@ require('dotenv').config();
 const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
 // https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
 const cn = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USER,
+  password: PG_PASSWORD,
 };
 
 // alt from express docs
