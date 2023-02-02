3b7408efb0b76f8de11e9b205d5ab01e2ad021c6
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-02-02 16:51:00 -0500

    commented out

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -4,15 +4,15 @@
 const pgp = require('pg-promise')();
 require('dotenv').config();
 
-const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, password } =
-  process.env;
+// const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, password } =
+//   process.env;
 // https://github.com/vitaly-t/pg-promise/wiki/Connection-Syntax#configuration-object
 const cn = {
-  host: PG_HOST,
-  port: PG_PORT,
-  database: PG_DATABASE,
-  user: PG_USER,
-  password: PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
 };
 
 // alt from express docs
